8a808a2d4dce3bf378075693d331b3e432473140
Sploob <crayiphone@gmail.com>
2022-01-28 01:20:27 -0800

    add error message when no answer is selected and the next button pressed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,46 @@
 $(document).ready(function() {
   $("#btnOne").click(function() {
-    $("#questOne").addClass("hidden")
-    $("#questTwo").removeClass("hidden")
+    if($("input[type='radio'][name='questOne']").is(":checked")) {
+      $("#questOne").addClass("hidden")
+      $("#questTwo").removeClass("hidden")
+      $("#error").addClass("hidden")
+    } else
+    $("#error").removeClass("hidden")
   });
   $("#btnTwo").click(function() {
-    $("#questTwo").addClass("hidden")
-    $("#questThree").removeClass("hidden")
+    if($("input[type='radio'][name='questTwo']").is(":checked")) {
+      $("#questTwo").addClass("hidden")
+      $("#questThree").removeClass("hidden")
+      $("#error").addClass("hidden")
+    } else {
+      $("#error").removeClass("hidden");
+    }
   });
   $("#btnThree").click(function() {
-    $("#questThree").addClass("hidden")
-    $("#questFour").removeClass("hidden")
+    if($("input[type='radio'][name='questThree']").is(":checked")) {
+      $("#questThree").addClass("hidden")
+      $("#questFour").removeClass("hidden")
+      $("#error").addClass("hidden")
+    } else {
+      $("#error").removeClass("hidden")
+    }
   });
   $("#btnFour").click(function() {
-    $("#questFour").addClass("hidden")
-    $("#questFive").removeClass("hidden")
+    if($("input[type='radio'][name='questFour']").is(":checked")) {
+      $("#questFour").addClass("hidden")
+      $("#questFive").removeClass("hidden")
+      $("#error").addClass("hidden")
+    } else {
+      $("#error").removeClass("hidden")
+    }
   });
 
-  $("form#quiz").submit(function() {
-    e.preventDefault();
+  $("form#quiz").submit(function(e) {
     const questOne = $("input[type='radio'][name='questOne']:checked").val();
     const questTwo = $("input[type='radio'][name='questTwo']:checked").val();
     const questThree = $("input[type='radio'][name='questThree']:checked").val();
     const questFour = $("input[type='radio'][name='questFour']:checked").val();
     const questFive = $("input[type='radio'][name='questFive']:checked").val();
+    e.preventDefault();
   });
-
 });
